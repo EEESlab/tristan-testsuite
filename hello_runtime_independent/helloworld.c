6e8768b1f1d8390de2e949002469af951eb98a74
Nazareno Bruschi <nazareno.bruschi@unibo.it>
2023-12-29 09:30:31 +0100

    Add runtime independent test

---
+++ hello_runtime_independent/helloworld.c
@@ -0,0 +1,15 @@
+/*
+ * TRISTAN TESTSUITE: HELLOWORLD
+ *
+ * Author: Nazareno Bruschi <nazareno.bruschi@unibo.it>
+ *
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello, world!\n");
+	return 0;
+}
+
